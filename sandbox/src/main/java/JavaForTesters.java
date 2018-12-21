e7b616b2c88d3a9f7a207e49d5ca26dc98715df8
Chathura <crajapakse@itsupportme.by>
2018-12-21 11:45:43 +0300

    создан проект для IDEA

---
+++ sandbox/src/main/java/JavaForTesters.java
@@ -0,0 +1,9 @@
+public class JavaForTesters{
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello,");
+        System.out.println("Home Task No. 01: Подготовить инфраструктуру и создать первую работающую программу");
+
+    }
+}
